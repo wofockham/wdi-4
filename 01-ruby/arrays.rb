74ee35dc3f3ac55ab78e9ec9ba2e93281c3610d3
Cold Head <certainthings@gmail.com>
2014-02-25 16:38:22 +1100

    more examples

---
+++ 01-ruby/arrays.rb
@@ -0,0 +1,12 @@
+a = []
+
+a = [33, '2', 1] # Array literal syntax
+
+puts a[0]
+puts a[1]
+puts a[-1]
+a[-1] == a[2] # True, negative indexes start from the end.
+
+a[3] = 'arble garble'
+puts a
+
---
+++ 01-ruby/truthhood.rb
@@ -0,0 +1,26 @@
+# Truthy values
+if ""
+  puts "the empty string is true"
+end
+
+if 0
+  puts "the number 0 is true"
+end
+
+
+if 1
+  puts "the number 1 is true"
+end
+
+if []
+  puts "the empty array is true"
+end
+
+# Falsy values
+if false
+  puts "the value false is true"
+end
+
+if nil
+  puts "nil is true"
+end
