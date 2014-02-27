40f7567de7e17c0cc2a8f82e9d3072b14bdda2d3
Cold Head <certainthings@gmail.com>
2014-02-27 11:35:32 +1100

    inheritance

--- 01-ruby/objects.rb
+++ 01-ruby/objects.rb
@@ -1,18 +1,55 @@
-class Person
+class Animal
   def speak
-    puts "Good morning"
+    puts "I am an animal"
   end
 
-  def talk(words_to_say)
-    puts "I like saying #{words_to_say}"
-  end
-
-  def age=(age)
-    @age = age
-  end
-
-  def age
-    @age
+  def move
+    puts "I can move"
   end
 end
+
+class Mammal < Animal
+  def eat
+    puts "I can eat nom nom nom"
+  end
+  def speak(action)
+    puts action
+    puts "I can speak"
+  end
+end
+
+class Monkey < Mammal
+  def swing
+    puts "I can swing"
+  end
+  def speak(action)
+    super
+    puts "HoWwOo WHoWo"
+  end
+end
+
+class Dog < Mammal
+  def speak
+    super("vomit")
+    puts "Woof"
+  end
+end
+
+class Person < Animal
+  attr_accessor :age, :name, :gender, :shoesize
+  def initialize(age, gender, name)
+    @age = age
+    @gender = gender
+    @name = name
+  end
+
+  def to_s
+    "#{@name} is a #{gender} of #{age} years. This code is bug free."
+  end
+
+  def Person.eye_count
+    2
+  end
+end
+
 
