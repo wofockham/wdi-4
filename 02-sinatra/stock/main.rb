a6efaac4074d9ce804837f8a7b2b677546c512ca
Cold Head <certainthings@gmail.com>
2014-03-04 09:41:27 +1100

    stock

--- 02-sinatra/stock/main.rb
+++ 02-sinatra/stock/main.rb
@@ -6,7 +6,7 @@ require 'active_support/all'
 
 get '/stock' do
   @symbol = params[:symbol]
-  if @symbol.present?
+  if @symbol.present? # Via active_support/all, this saves us comparing @symbol != ''
     @symbol.upcase!
     @price = YahooFinance::get_quotes(YahooFinance::StandardQuote, @symbol)[@symbol].lastTrade
   end
