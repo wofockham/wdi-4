9be5cd0a03878b38de58206e9b290b7e988e849b
Cold Head <certainthings@gmail.com>
2014-03-05 11:36:11 +1100

    single resource, multiple verbs

--- 02-sinatra/verbs/main.rb
+++ 02-sinatra/verbs/main.rb
@@ -10,6 +10,15 @@ get '/contact' do
   erb :contact
 end
 
-post '/processform' do
-  binding.pry
+post '/contact' do
+  # TODO: Use params data to send email to our sales team.
+  erb :thankyou
+end
+
+get '/search' do
+  "You searched for #{ params[:query] }. There are #{ rand(100) } results."
+end
+
+post '/subscribe' do
+  "Thank you, your payment has been processed and we have all your money."
 end
