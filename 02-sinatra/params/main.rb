6536ef47f7ab8a428c01a9d93bd4fe4855b3fdc8
Cold Head <certainthings@gmail.com>
2014-03-07 15:13:14 +1100

    thanks for asking about params, people

--- 02-sinatra/params/main.rb
+++ 02-sinatra/params/main.rb
@@ -2,30 +2,24 @@ require 'pry'
 require 'sinatra'
 require 'sinatra/reloader'
 
-def flubber(xyz)
-  puts "message from flubber: #{ xyz }"
+get '/hello' do
+  "hello world"
 end
 
-flubber('literally') # => message from flubber: literally
-flubber 'literally' # => message from flubber: literally
-
-teepee = :hotdogs
-flubber(teepee) # => message from flubber: hotdogs
-flubber('teepee') # => message from flubber: teepee
-flubber(:hotdogs) # => message from flubber: hotdogs
-
-xyz = 8002
-flubber(teepee) # => message from flubber: hotdogs
-
-pq = 'this is bad'
-def test
-  puts pq
+get '/students/:first_name' do
+  @student_name = params[:first_name]
+  erb :student # <%= @student_name %> || <%= params[:first_name] %>
 end
 
-class Sandwich
-  @slices_of_bread = 2
-
-  def how_much_bread
-    @slices_of_bread
-  end
+get '/form' do
+  erb :form
 end
+
+get '/action' do
+  binding.pry
+end
+
+post '/action' do
+  binding.pry
+end
+
