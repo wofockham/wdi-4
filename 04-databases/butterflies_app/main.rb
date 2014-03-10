b3085f10ead1727bddbe1d5186aa73ae97bf3a71
Cold Head <certainthings@gmail.com>
2014-03-10 13:01:58 +1100

    create and a little bit of read

--- 04-databases/butterflies_app/main.rb
+++ 04-databases/butterflies_app/main.rb
@@ -1,9 +1,16 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
+require 'pry'
 
 get '/' do
   erb :home
+end
+
+get '/butterflies' do
+  db = SQLite3::Database.open('butterflies.db')
+  @butterflies = db.execute('SELECT * FROM butterflies')
+  erb :butterflies
 end
 
 get '/butterflies/new' do
@@ -11,5 +18,12 @@ get '/butterflies/new' do
 end
 
 post '/butterflies/create' do
+  db = SQLite3::Database.open('butterflies.db')
 
+  sql = "INSERT INTO butterflies (name, family, photo)
+          VALUES ('#{params[:name]}', '#{params[:family]}', '#{params[:photo]}');"
+
+  db.execute sql
+
+  redirect to('/butterflies')
 end
