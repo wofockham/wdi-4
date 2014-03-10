23f034169e339079b5800b412f3f8e85482f1d45
Cold Head <certainthings@gmail.com>
2014-03-10 15:18:15 +1100

    full crud gsub horror

--- 04-databases/butterflies_app/main.rb
+++ 04-databases/butterflies_app/main.rb
@@ -3,13 +3,16 @@ require 'sinatra/reloader'
 require 'sqlite3'
 require 'pry'
 
+before do
+  @families = query_db('SELECT DISTINCT family FROM butterflies;')
+end
+
 get '/' do
   erb :home
 end
 
 get '/butterflies' do
-  db = SQLite3::Database.open('butterflies.db')
-  @butterflies = db.execute('SELECT * FROM butterflies;')
+  @butterflies = query_db('SELECT * FROM butterflies;')
   erb :butterflies
 end
 
@@ -18,12 +21,46 @@ get '/butterflies/new' do
 end
 
 post '/butterflies/create' do
-  db = SQLite3::Database.open('butterflies.db')
-
   sql = "INSERT INTO butterflies (name, family, photo)
           VALUES ('#{params[:name]}', '#{params[:family]}', '#{params[:photo]}');"
-
-  db.execute sql
-
+  query_db(sql)
   redirect to('/butterflies')
+end
+
+post '/butterflies/update' do
+  sql = "UPDATE butterflies SET
+    name='#{params[:name].gsub("'", "''")}',
+    family='#{params[:family].gsub("'", "''")}',
+    photo='#{params[:photo].gsub("'", "''")}' WHERE id=#{params[:id]}"
+  query_db(sql)
+  redirect to("/butterflies/#{params[:id]}")
+end
+
+get '/butterflies/:id' do
+  results = query_db("SELECT * FROM butterflies WHERE id=#{ params[:id] }")
+  @butterfly = results.first
+  erb :butterfly
+end
+
+get '/butterflies/:id/edit' do
+  results = query_db("SELECT * FROM butterflies WHERE id=#{ params[:id] }")
+  @butterfly = results.first
+  erb :edit_butterfly
+end
+
+get '/butterflies/:id/delete' do
+  query_db("DELETE FROM butterflies WHERE id=#{ params[:id] }")
+  redirect to('/butterflies')
+end
+
+get '/butterflies/family/:family' do
+  @butterflies = query_db("SELECT * FROM butterflies WHERE family LIKE '#{params[:family]}'")
+  erb :butterflies
+end
+
+def query_db(sql)
+  db = SQLite3::Database.open('butterflies.db')
+  db.results_as_hash = true
+  puts "SQL: #{ sql }"
+  db.execute(sql)
 end
