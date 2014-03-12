4bad5c7b235ea7e5198ac905240ed0f11f92a193
Cold Head <certainthings@gmail.com>
2014-03-12 12:40:30 +1100

    raw db version

--- 04-databases/students/main.rb
+++ 04-databases/students/main.rb
@@ -2,6 +2,14 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'pg'
 
+before do
+  $db = PG.connect(:dbname =>'students', :host => 'localhost')
+end
+
+after do
+  $db.close if $db # Good housekeeping.
+end
+
 get '/' do
   @courses = query_db('SELECT * FROM courses;')
   @students = query_db('SELECT * FROM students;')
@@ -9,15 +17,22 @@ get '/' do
 end
 
 get '/courses/:id' do
+  @course = query_db("SELECT * FROM courses WHERE id=#{ params[:id] }").first
+  @students = query_db("SELECT * FROM students WHERE id IN (
+                          SELECT student_id FROM courses_students WHERE course_id=#{ params[:id]}
+                        )")
+  erb :course
 end
 
 get '/students/:id' do
+  @student = query_db("SELECT * FROM students WHERE id=#{ params[:id] }").first
+  @courses = query_db("SELECT * FROM courses WHERE id IN (
+                        SELECT course_id FROM courses_students WHERE student_id=#{params[:id]}
+                       )")
+  erb :student
 end
 
 def query_db(sql)
-  db = PG.connect(:dbname =>'students', :host => 'localhost')
   puts "SQL: #{ sql }"
-  results = db.exec(sql)
-  db.close
-  results
+  $db.exec(sql)
 end
