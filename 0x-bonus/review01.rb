cb6a967ab99cd1cce411539d54ec52ba240e1d82
Cold Head <certainthings@gmail.com>
2014-03-13 17:30:02 +1100

    beetlejuice

---
+++ 0x-bonus/review01.rb
@@ -0,0 +1,83 @@
+array = [2, 4, 8, 16, 53]
+array << 7
+array << -1001
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+x = 'i do not really have a favourite number' # VERY IMPORTANT TO EMAIL TO RILEY
+
+array.each do |favourite_number|
+  puts "joel has a favourite number #{ favourite_number }"
+end
+
+puts x
+
+10.downto(-10) do |i|
+  puts "#{i}..."
+end
+puts "blastoff!"
+
+3.times do
+  puts "beetlejuice"
+end
