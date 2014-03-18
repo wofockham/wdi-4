a6d6fb7eefe55690ddc8c7c1ddeb40ac71349f60
Cold Head <certainthings@gmail.com>
2014-03-18 14:23:07 +1100

    planet edit form

--- 05-rails/solar_system/app/controllers/planets_controller.rb
+++ 05-rails/solar_system/app/controllers/planets_controller.rb
@@ -16,4 +16,14 @@ class PlanetsController < ApplicationController
   def show
     @planet = Planet.find params[:id]
   end
+
+  def edit
+    @planet = Planet.find params[:id]
+  end
+
+  def destroy
+    planet = Planet.find params[:id]
+    planet.destroy
+    redirect_to planets_path
+  end
 end
--- 05-rails/solar_system/config/routes.rb
+++ 05-rails/solar_system/config/routes.rb
@@ -4,4 +4,7 @@ SolarSystem::Application.routes.draw do
   get '/planets/new' => 'planets#new'
   post '/planets' => 'planets#create'
   get '/planets/:id' => 'planets#show', :as => :planet
+  post '/planets/:id/delete' => 'planets#destroy', :as => :planet_delete
+  get '/planets/:id/edit' => 'planets#edit', :as => :planet_edit
+  post '/planets/:id' => 'planets#update'
 end
