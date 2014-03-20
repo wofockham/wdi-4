3ba119eba68c745d8e3fdd6da7fa40e16008ac85
Cold Head <certainthings@gmail.com>
2014-03-20 15:08:54 +1100

    associations on the front-end

--- 05-rails/cookbook/app/models/recipe.rb
+++ 05-rails/cookbook/app/models/recipe.rb
@@ -15,7 +15,7 @@
 #
 
 class Recipe < ActiveRecord::Base
-  attr_accessible :name, :course, :cooktime, :servingsize, :instructions, :image, :book_id
+  attr_accessible :name, :course, :cooktime, :servingsize, :instructions, :image, :book_id, :ingredient_ids
   belongs_to :book
   has_and_belongs_to_many :ingredients
 end
--- 05-rails/cookbook/config/routes.rb
+++ 05-rails/cookbook/config/routes.rb
@@ -1,3 +1,4 @@
 Cookbook::Application.routes.draw do
+  root :to => 'books#index'
   resources :books, :recipes, :ingredients
 end
