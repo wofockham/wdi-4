c80247a6575764cc9217bdad5b04cd83b9ce3ff4
Cold Head <certainthings@gmail.com>
2014-03-20 12:32:09 +1100

    book CRUD

---
+++ 05-rails/cookbook/app/controllers/books_controller.rb
@@ -0,0 +1,34 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+  end
+
+  def create
+    book = Book.create(params[:book])
+    redirect_to book
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def edit
+    @book = Book.find params[:id]
+  end
+
+  def show
+    @book = Book.find params[:id]
+  end
+
+  def update
+    book = Book.find params[:id]
+    book.update_attributes params[:book]
+    redirect_to book
+  end
+
+  def destroy
+    book = Book.find params[:id]
+    book.destroy
+    redirect_to books_path
+  end
+end
