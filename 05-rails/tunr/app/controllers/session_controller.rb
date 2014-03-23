0fec7e6235572640ada7d6198c4ca29484232241
Cold Head <certainthings@gmail.com>
2014-03-24 10:32:12 +1100

    login works

---
+++ 05-rails/tunr/app/controllers/session_controller.rb
@@ -0,0 +1,20 @@
+class SessionController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.where(:name => params[:name]).first
+    if user.present? && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      session[:user_id] = nil
+      redirect_to login_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+end
--- 05-rails/tunr/config/routes.rb
+++ 05-rails/tunr/config/routes.rb
@@ -1,4 +1,7 @@
 Tunr::Application.routes.draw do
   root :to => 'pages#index'
+  get '/login' => 'session#new'
+  post '/login' => 'session#create'
+  delete '/login' => 'session#destroy'
   resources :users
 end
