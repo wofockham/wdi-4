aad1edf4dbd0fe3b4e02d965252fa0507f77ccd6
Cold Head <certainthings@gmail.com>
2014-03-24 10:44:38 +1100

    intellinav

--- 05-rails/tunr/app/helpers/application_helper.rb
+++ 05-rails/tunr/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def intellinav
+    if @current_user
+      "<li>#{ link_to('Log out ' + @current_user.name, login_path, :method => :delete, :confirm => 'Really log out?') }</li>"
+    else
+      "<li>#{ link_to('Sign up', new_user_path) }</li>
+       <li>#{ link_to('Sign in', login_path) }</li>"
+    end
+  end
 end
