68d6486d6cc774d172f1afb97345a1ddace77991
Cold Head <certainthings@gmail.com>
2014-03-25 12:33:13 +1100

    geocoding demo

--- 05-rails/tunr/app/models/user.rb
+++ 05-rails/tunr/app/models/user.rb
@@ -9,13 +9,19 @@
 #  updated_at      :datetime
 #  password_digest :string(255)
 #  admin           :boolean          default(FALSE)
+#  address         :text
+#  latitude        :float
+#  longitude       :float
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :name, :image, :password, :password_confirmation
+  attr_accessible :name, :image, :password, :password_confirmation, :address
   has_many :mixtapes
 
   has_secure_password
   validates :image, :presence => true
   validates :name, :presence => true, :uniqueness => true, :length => { :minimum => 2 }
+
+  geocoded_by :address
+  after_validation :geocode
 end
--- 05-rails/tunr/db/schema.rb
+++ 05-rails/tunr/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140323234541) do
+ActiveRecord::Schema.define(version: 20140325011313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,9 @@ ActiveRecord::Schema.define(version: 20140323234541) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.boolean  "admin",           default: false
+    t.text     "address"
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
 end
