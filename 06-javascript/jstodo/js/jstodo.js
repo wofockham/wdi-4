5812c47cee6fd2fefc3f104c8aca8573f7d85e59
Cold Head <certainthings@gmail.com>
2014-04-07 12:23:16 +1000

    using _.template()

--- 06-javascript/jstodo/js/jstodo.js
+++ 06-javascript/jstodo/js/jstodo.js
@@ -8,4 +8,56 @@ $(document).ready(function () {
     });
   });
 
+  $('form.task_creator').on('submit', function (event) {
+    event.preventDefault();
+    var $input = $('#new_task');
+    var description = $input.val();
+    $input.val('');
+    console.log('ready to create new task', description);
+    jsTodoApp.createTask(description);
+    jsTodoApp.showTasks();
+    console.log(jsTodoApp.tasks);
+  });
 });
+
+var jsTodoApp = {
+  tasks: [],
+
+  createTask: function (description) {
+    var task = {
+      'description': description,
+      createdAt: (new Date()).toString()
+    };
+
+    this.tasks.push(task);
+    return task;
+  },
+
+  showTasks: function () {
+    var task_format = $('#task_template').html();
+    var task_html = _.template(task_format);
+
+    _.each(this.tasks, function (task) {
+      var html = task_html(task);
+      $(html).appendTo('#tasks');
+    });
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
