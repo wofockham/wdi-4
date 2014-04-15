cfd1551fcc3ce2ee3b7e16f1c1e5afd86960575f
Cold Head <certainthings@gmail.com>
2014-04-15 16:36:27 +1000

    cucumber registration tests

---
+++ 05-rails/cuky/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cuky::Application.config.secret_key_base = '0b8949feab0347ebe38bc496f1198185dd53fe4f0512f82214892e68b18ad6ce265d2de681327c1fc124c03e2e2e39fbb06e11929441f30634fdabde4256359c'
---
+++ 05-rails/cuky/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cuky::Application.config.session_store :cookie_store, key: '_cuky_session'
