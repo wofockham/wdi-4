26fc65451ae84efc6ad1dae464039f48440d2a9a
Cold Head <certainthings@gmail.com>
2014-04-16 15:29:33 +1000

    SCSS demo

---
+++ 05-rails/bloge_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BlogeApp::Application.config.secret_key_base = '89c14d5d314c7f97c361e1f85e1bd07e5553327dcc21c32e458a2561021f11993dc2e9cc0fe9b504a4f9da8e57f2f5019cf9ca210e648b4503115df04612d6ea'
